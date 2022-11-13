5eea008bc5c650047663a367e2c5ff98efeac4ed
Ary Setiyawan <arysetiyawan@sindata.net>
2022-11-13 19:02:56 +0800

    Add soal2.js

---
+++ soal2.js
@@ -0,0 +1,56 @@
+let productBin = {
+  "requestId": null,
+  "data": [
+    {
+      "id": 100000057465,
+      "storageId": 10000008207,
+      "code": "A01-01-01-A",
+      "productId": 110000081009,
+      "productName": "FloBrand-DressBSPink",
+      "productCode": "FBR00040101",
+      "quantity": 76,
+      "createdTime": "2021-12-21T13:54:48Z",
+    },
+    {
+      "id": 100000057466,
+      "storageId": 10000002181,
+      "code": "A01-01-01-B",
+      "productId": 110000081009,
+      "productName": "FloBrand-DressBSPink",
+      "productCode": "FBR00040101",
+      "quantity": 71,
+      "createdTime": "2021-12-21T13:54:48Z",
+    },
+    {
+      "id": 100000065224,
+      "storageId": 10000008884,
+      "code": "Tgt00-A-A-01",
+      "productId": 110000081009,
+      "productName": "FloBrand-DressBSPink",
+      "productCode": "FBR00040101",
+      "quantity": 10,
+      "createdTime": "2022-02-08T10:35:19Z",
+    }
+  ],
+  "message": "success"
+};
+
+const getTotalQty = (params) => {
+  let totalQty = 0;
+
+  if (!params || !params?.data) {
+    totalQty = 0;
+  } else {
+    params.data.forEach(product => {
+      if (!product?.quantity || isNaN(product.quantity)) {
+        totalQty += 0;
+      } else {
+        totalQty += product.quantity;
+      }
+    });
+  }
+
+  return totalQty;
+};
+
+console.log(getTotalQty(productBin));
